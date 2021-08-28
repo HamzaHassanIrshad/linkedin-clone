219bf0f5da2f35dabc00cea5ce6dc0b9c9df6f23
HamzaHassanIrshad <hamza.2019701@iit.ac.lk>
2021-08-28 20:23:26 +0530

    sidebar completed

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -3,10 +3,19 @@ import React from "react";
 import "./Sidebar.css";
 
 function Sidebar() {
+  const recentItem = (topic) => (
+    <div className="sidebar__recentItem">
+      <span className="sidebar__hash">#</span>
+      <p>{topic}</p>
+    </div>
+  );
   return (
     <div className="sidebar">
       <div className="sidebar__top">
-        <img src="" alt="" />
+        <img
+          src="https://media-exp1.licdn.com/dms/image/C5616AQHx9Zsvb4QDUQ/profile-displaybackgroundimage-shrink_350_1400/0/1624743388138?e=1635379200&v=beta&t=A2yYz3Lg5A5GipPQLHUzENdVen1-2U3fjCFdbvXdXlI"
+          alt="background-image"
+        />
         <Avatar className="sidebar__avatar" />
         <h2>Hamza Hassan</h2>
         <h4>hamza.hassan00321@gmail.com</h4>
@@ -23,6 +32,11 @@ function Sidebar() {
       </div>
       <div className="sidebar__bottom">
         <p>Recent</p>
+        {recentItem("reactjs")}
+        {recentItem("programming")}
+        {recentItem("softwareengineering")}
+        {recentItem("desing")}
+        {recentItem("developer")}
       </div>
     </div>
   );
