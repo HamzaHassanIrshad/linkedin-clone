aa8ca10c0d24afadbc8a3b07101e60b7880bfd60
HamzaHassanIrshad <hamza.2019701@iit.ac.lk>
2021-08-29 18:52:22 +0530

    logout, login done

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,8 +7,17 @@ import SupervisorAccountIcon from "@material-ui/icons/SupervisorAccount";
 import BusinessCenterIcon from "@material-ui/icons/BusinessCenter";
 import ChatIcon from "@material-ui/icons/Chat";
 import NotificationsIcon from "@material-ui/icons/Notifications";
+import { useDispatch } from "react-redux";
+import { logout } from "../features/userSlice";
+import { auth } from "../firebase";
 
 function Header() {
+  const dispatch = useDispatch();
+
+  const logOutOfApp = () => {
+    dispatch(logout());
+    auth.signOut();
+  };
   return (
     <div className="header">
       <div className="header__left">
@@ -18,7 +27,7 @@ function Header() {
         />
         <div className="header__search">
           <SearchIcon />
-          <input type="text" />
+          <input placeholder="Search" type="text" />
         </div>
       </div>
       <div className="header__right">
@@ -30,6 +39,7 @@ function Header() {
         <HeaderOption
           avatar="https://media-exp1.licdn.com/dms/image/C5603AQGwoDzcqSXQNg/profile-displayphoto-shrink_200_200/0/1601472017650?e=1635379200&v=beta&t=-LlDV9nT3p8Cc9MSzxmM28Vd6XwIFc30o-rBFbqdrHE"
           title="Me"
+          onClick={logOutOfApp}
         />
       </div>
     </div>
