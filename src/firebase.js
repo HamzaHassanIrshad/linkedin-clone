8f216edad30d602bd024a9a82ead4641f273b0d6
HamzaHassanIrshad <hamzahassanoff@gmail.com>
2023-03-14 01:57:31 +0530

    bug fixes

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,12 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDGf0fKA3xkykDYQ8rrcR2XxNY-UZ3eSSs",
-  authDomain: "linkedin-clone-ab6d0.firebaseapp.com",
-  projectId: "linkedin-clone-ab6d0",
-  storageBucket: "linkedin-clone-ab6d0.appspot.com",
-  messagingSenderId: "764000709151",
-  appId: "1:764000709151:web:2a623758f2273e5602832a",
+  apiKey: "AIzaSyC_1UwKUed6AVOsFxn2MR0-YsQyri7gRZs",
+  authDomain: "linkedin-clone-d90de.firebaseapp.com",
+  projectId: "linkedin-clone-d90de",
+  storageBucket: "linkedin-clone-d90de.appspot.com",
+  messagingSenderId: "247264052878",
+  appId: "1:247264052878:web:93e0ccdc7653f7ed44abdf",
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
